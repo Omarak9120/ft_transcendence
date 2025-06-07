56120e97dc7724e0629a2ef764668af57d37d0f8
Marc Katerji <mkaterji@c1r3s4.42beirut.com>
2025-06-07 18:50:03 +0300

    made apis dynamic

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,10 +1,16 @@
-import { FastifyInstance } from 'fastify';
+// routes/stats.ts
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import db from '../utils/db';
+import { authMiddleware } from '../middleware/auth';
+import { JWTPayload } from '../utils/jwt';
 
 export default async function userRoutes(fastify: FastifyInstance) {
 
-  fastify.get('/api/users/me/trophies', async (req, reply) => {
-    const userId = 7; // 🔒 replace with getUserId(req) later
+  fastify.get('/api/users/me/trophies',
+    { preHandler: authMiddleware },
+    async (req: FastifyRequest, reply: FastifyReply) => {
+      // locally assert we ran authMiddleware and set `req.user`
+      const { userId } = (req as FastifyRequest & { user: JWTPayload }).user;
 
     const user = db.prepare(`SELECT trophies FROM users WHERE id = ?`).get(userId);
     if (!user) {
